d01d414453b20d4f312cc1e959bb16f102fe67c0
Fermin Garcia <fermin.f.garcia1@gmail.com>
2023-03-13 08:57:00 -0500

    cleared up code and added files

--- acquire.py
+++ acquire.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import  os
+import os
 import env
 
 def get_connection(db, user=env.user, host=env.host, password=env.password):
@@ -13,7 +13,6 @@ def get_titanic_data():
         titanic_df.to_csv('titanic.csv')
         return pd.read_csv('titanic.csv')
 
-
 iris_sql="SELECT measurements.measurement_id, \
     measurements.sepal_length, measurements.sepal_width, measurements.petal_length, \
         measurements.petal_width, species.species_name, species.species_id \
@@ -21,7 +20,6 @@ iris_sql="SELECT measurements.measurement_id, \
 
 def get_iris_data():
     if os.path.isfile('iris.csv') == True:
-  
         return pd.read_csv('iris.csv')
     else:
          iris_df = pd.read_sql(iris_sql,get_connection('iris_db'))
@@ -30,15 +28,17 @@ def get_iris_data():
     
 
 
-telco_sql= " select * \
-from customers \
-join customer_contracts using(customer_id) \
-join internet_service_types using(internet_service_type_id) \
-join payment_types using(payment_type_id) "
+telco_sql= """select payment_type, contract_type, internet_service_type, customers.customer_id gender,senior_citizen, partner, dependents, tenure, phone_service, multiple_lines, online_security, online_backup, device_protection, tech_support, streaming_tv, streaming_movies, customers.paperless_billing
+from customers 
+join customer_contracts using(contract_type_id)
+join internet_service_types using(internet_service_type_id)
+join payment_types using(payment_type_id)
+join contract_types using(contract_type_id)"""
 
 def get_telco_data():
     if os.path.isfile('telco.csv') == True:
         return pd.read_csv('telco.csv')
     else:
-         df = pd.read_sql(telco_sql,get_connection('telco_churn')),
-         return df.read_csv('telco.csv')
+        telco_df = pd.read_sql(telco_sql,get_connection('telco_churn'))
+        df = telco_df.to_csv('telco.csv')
+        return df.read_csv('telco.csv')
---
+++ prepare.py
@@ -0,0 +1,57 @@
+def prep_iris (): 
+    import aquire as a
+    if 'measurement_id' in iris.columns:
+        dropcols = ['species_id','measurement_id']
+    else:
+        dropcols = ['species_id']
+    return iris.drop(
+        columns=dropcols).rename(
+        columns={'species_name': 'species'})
+
+def wrangle_iris():
+    return prep_iris(acquire.get_iris_data())
+
+
+
+
+
+
+def prep_titanic(df):
+    df = df.drop(columns=['passenger_id','embarked','deck','class'])
+    df['age'] = df['age'].fillna(df.age.mean())
+    df['embark_town'] = df['embark_town'].fillna('Southampton')
+    df = pd.concat(
+    [df, pd.get_dummies(df[['sex', 'embark_town']],
+                        drop_first=True)], axis=1)
+    return df
+
+
+
+
+
+def prep_telco(telco):
+    telco['total_charges'] = (telco.total_charges + '0').astype(float)
+    telco = telco.drop(columns=['internet_service_type_id', 'contract_type_id', 'payment_type_id'])
+
+    telco['gender_encoded'] = telco.gender.map({'Female': 1, 'Male': 0})
+    telco['partner_encoded'] = telco.partner.map({'Yes': 1, 'No': 0})
+    telco['dependents_encoded'] = telco.dependents.map({'Yes': 1, 'No': 0})
+    telco['phone_service_encoded'] = telco.phone_service.map({'Yes': 1, 'No': 0})
+    telco['paperless_billing_encoded'] = telco.paperless_billing.map({'Yes': 1, 'No': 0})
+    telco['churn_encoded'] = telco.churn.map({'Yes': 1, 'No': 0})
+    
+    dummy_df = pd.get_dummies(telco[['multiple_lines', \
+                              'online_security', \
+                              'online_backup', \
+                              'device_protection', \
+                              'tech_support', \
+                              'streaming_tv', \
+                              'streaming_movies', \
+                              'contract_type', \
+                              'internet_service_type', \
+                              'payment_type'
+                            ]],
+                              drop_first=True)
+    telco = pd.concat( [telco, dummy_df], axis=1 )
+    
+    return telco
